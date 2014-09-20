07dd2f613c7a8c676320667fac47c6ea821cc787
Igor Kapkov <igasgeek@me.com>
2014-09-20 20:52:46 +0800

    cleanup

--- lib/capistrano/wizard/helpers/template.rb
+++ lib/capistrano/wizard/helpers/template.rb
@@ -5,8 +5,10 @@ module Capistrano
     module Helpers
       module Template
         def make_template(source, destination)
-          source = File.expand_path(File.join(File.dirname(__FILE__),"..", source))
-          File.open(destination, 'wb') { |f| f.write ERB.new(File.binread(source)).result(instance_eval('binding')) }
+          source = File.expand_path(File.join(File.dirname(__FILE__), "..", source))
+          File.open(destination, 'wb') do |f|
+            f.write ERB.new(File.binread(source)).result(instance_eval('binding'))
+          end
         end
       end
     end
